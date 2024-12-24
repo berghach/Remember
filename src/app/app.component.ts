914255d492e9982a73c4f1774a871fbb9f0265f0
berghach <berghach.chemseddine@gmail.com>
2024-12-24 11:33:58 +0100

    understanding angular architecture & set upthe header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MemoryGameModule } from './memory-game/memory-game.module';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MemoryGameModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
